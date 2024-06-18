31e60c5b07a4137da50c72bea87eade0fa5ac217
Jordane Chaves <jordane_chaves@yahoo.com.br>
2024-06-17 21:44:01 -0300

    feat: initial configuration

---
+++ src/styles/global.ts
@@ -0,0 +1,39 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  body {
+    background-color: ${(props) => props.theme.colors.background};
+  }
+
+  body, button, input, textarea {
+    font-family: 'Roboto', sans-serif;
+    font-size: 1rem;
+    font-weight: 400;
+  }
+
+  h1, h2, h3 {
+    font-family: "Baloo 2", sans-serif;
+    font-weight: 800;
+    line-height: 130%;
+  }
+
+  h1 {
+    font-size: 3rem;
+  }
+
+  h2 {
+    font-size: 2rem;
+  }
+
+  h3 {
+    font-size: 1.5rem;
+  }
+`
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,26 @@
+export const defaultTheme = {
+  colors: {
+    white: '#ffffff',
+    background: '#faf9f9',
+    card: '#f2f1f1',
+    input: '#edecec',
+    button: '#e6e5e5',
+    hover: '#d7d5d5',
+    label: '#8c8585',
+    text: '#564f4c',
+    subtitle: '#3f3937',
+    title: '#262220',
+
+    brand: {
+      100: '#ebe5f9',
+      500: '#7f46f7',
+      900: '#4b2894',
+    },
+
+    yellow: {
+      100: '#f0e8c9',
+      500: '#dbac2b',
+      700: '#c47e16',
+    },
+  },
+}
