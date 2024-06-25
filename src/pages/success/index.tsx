5c085fd264be8ed9683cc8ccbc18e1b9b7c6fec0
Jordane Chaves <jordane_chaves@yahoo.com.br>
2024-06-24 22:27:14 -0300

    feat: create success page

--- src/pages/success/index.tsx
+++ src/pages/success/index.tsx
@@ -1,9 +1,59 @@
-import { SuccessContainer } from './styles'
+import { PiCurrencyDollar, PiMapPinFill, PiTimerFill } from 'react-icons/pi'
+
+import deliverymanImage from '../../assets/deliveryman.png'
+import {
+  InfoItem,
+  OrderInfo,
+  OrderInfoContainer,
+  SuccessContainer,
+} from './styles'
 
 export function Success() {
   return (
     <SuccessContainer className="container">
-      <h1>Pedido confirmado</h1>
+      <h2>Uhu! Pedido confirmado</h2>
+      <p>Agora é só aguardar que logo o café chegará até você</p>
+
+      <OrderInfoContainer>
+        <OrderInfo>
+          <InfoItem $color="purple">
+            <div className="icon">
+              <PiMapPinFill />
+            </div>
+            <div>
+              <p>
+                Entrega em <strong>Rua João Daniel Martinelli, 102</strong>
+              </p>
+              <p>Farrapos - Porto Alegre, RS</p>
+            </div>
+          </InfoItem>
+
+          <InfoItem $color="yellow">
+            <div className="icon">
+              <PiTimerFill />
+            </div>
+            <div>
+              <p>Previsão de entrega</p>
+              <strong>20 min - 30 min</strong>
+            </div>
+          </InfoItem>
+
+          <InfoItem $color="yellow-dark">
+            <div className="icon">
+              <PiCurrencyDollar />
+            </div>
+            <div>
+              <p>Pagamento na entrega</p>
+              <strong>Cartão de Crédito</strong>
+            </div>
+          </InfoItem>
+        </OrderInfo>
+
+        <img
+          src={deliverymanImage}
+          alt="Ilustração de um homem pilotando uma scooter com uma caixa atrás"
+        />
+      </OrderInfoContainer>
     </SuccessContainer>
   )
 }
--- src/pages/success/styles.ts
+++ src/pages/success/styles.ts
@@ -1,3 +1,120 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-export const SuccessContainer = styled.div``
+export const SuccessContainer = styled.main`
+  padding-bottom: 4rem;
+
+  > h2 {
+    color: ${(props) => props.theme.colors['yellow-dark']};
+
+    margin-top: 5rem;
+  }
+
+  > p {
+    color: ${(props) => props.theme.colors.subtitle};
+    font-size: 1.25rem;
+
+    margin-top: 0.25rem;
+  }
+
+  @media (max-width: ${(props) => props.theme.screens.md}) {
+    > h2 {
+      margin-top: 2rem;
+    }
+  }
+`
+
+export const OrderInfoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 1rem;
+
+  margin-top: 2.5rem;
+
+  img {
+    object-fit: contain;
+    height: auto;
+    max-width: 30.75rem;
+    min-width: 12rem;
+  }
+
+  @media (max-width: ${(props) => props.theme.screens.md}) {
+    flex-direction: column-reverse;
+    margin-top: 3rem;
+
+    img {
+      max-width: 70vw;
+    }
+  }
+`
+
+export const OrderInfo = styled.div`
+  background-color: ${(props) => props.theme.colors.background};
+  border-radius: 6px 36px;
+
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+
+  max-width: 32.875rem;
+  min-width: min(20rem, 100%);
+  width: 100%;
+
+  padding: 2.5rem;
+
+  position: relative;
+
+  &::before {
+    content: '';
+
+    border-radius: inherit;
+    background: ${(props) =>
+      css`
+        linear-gradient(
+          -0.63turn,
+          ${props.theme.colors.yellow},
+          ${props.theme.colors.purple}
+        )
+      `};
+
+    margin: -1px;
+    position: absolute;
+    inset: 0;
+    z-index: -1;
+  }
+
+  @media (max-width: ${(props) => props.theme.screens.md}) {
+    max-width: 100%;
+  }
+`
+
+interface InfoItemProps {
+  $color: 'purple' | 'yellow' | 'yellow-dark'
+}
+
+export const InfoItem = styled.div<InfoItemProps>`
+  color: ${(props) => props.theme.colors.text};
+  font-size: 1rem;
+
+  display: flex;
+  align-items: center;
+  gap: 0.75rem;
+
+  .icon {
+    background-color: ${(props) => props.theme.colors[props.$color]};
+    border-radius: 50%;
+    color: ${(props) => props.theme.colors.background};
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    padding: 0.5rem;
+    width: fit-content;
+
+    svg {
+      height: 1rem;
+      width: 1rem;
+    }
+  }
+`
